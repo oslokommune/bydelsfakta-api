43575363c447ce0500bf06edadd68e628aa1d7f2
torklo <tord.kloster@knowit.no>
2019-06-18 10:45:13 +0200

    add xray tracing

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import re
 import boto3
 import requests
 import logging
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch
+
+patch(['boto3'])
+patch(['requests'])
 
 metadata_api = os.environ["METADATA_API_URL"]
 bucket = "ok-origo-dataplatform-{}".format(os.environ["STAGE"])
@@ -18,8 +23,9 @@ def handler(event, context):
         return {"statusCode": 403, "body": "Forbidden: Only bydelsfakta frontend is allowed to use this api"}
     return handle_event(event)
 
-
+@xray_recorder.capture('handle_event')
 def handle_event(event):
+    @xray_recorder.capture('get_objects')
     def gen_lists():
         keys = []
         if not query:
@@ -73,7 +79,7 @@ def handle_event(event):
 
     return {"statusCode": 200, "headers": {"Content-Type": "application/json"}, "body": json.dumps(data, ensure_ascii=False)}
 
-
+@xray_recorder.capture('get_version')
 def get_latest_version(dataset_id):
     all_versions = requests.get(f"{metadata_api}/datasets/{dataset_id}/versions")
     all_versions = json.loads(all_versions.text)
@@ -84,7 +90,7 @@ def get_latest_version(dataset_id):
     latest_version = max(all_versions, key=lambda x: x["version"] if "version" in x else -1)
     return latest_version["version"]
 
-
+@xray_recorder.capture('get_edition')
 def get_latest_edition(dataset_id, version):
     all_editions = requests.get(f"{metadata_api}/datasets/{dataset_id}/versions/{version}/editions")
     all_editions = json.loads(all_editions.text)
